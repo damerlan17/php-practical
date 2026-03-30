df65963ca491e40a6ae8e146c2ee2effd0750043
damerlan17 <uchunakdamelan@gmail/com>
2026-03-30 21:37:13 +0700

    add composer.json

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -2,6 +2,8 @@
 //Путь до директории с конфигурационными файлами
 const DIR_CONFIG = '/../config';
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
 //Добавляем пользовательскую функцию автозагрузки классов
 spl_autoload_register(function ($className) {
     $paths = include __DIR__ . DIR_CONFIG . '/path.php';
