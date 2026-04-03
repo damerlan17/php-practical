63395e4e10573ad9ffa324a90919a1926105ecca
damerlan17 <uchunakdamelan@gmail/com>
2026-04-03 12:07:21 +0700

    подключил phpmyadmin дома

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'mvc',
+    'username' => 'root',
+    'password' => 'Uchunak-2006',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
