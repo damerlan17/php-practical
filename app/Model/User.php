113333cdc6216ff037c0f9fe52755577c2a6c316
gr441_uaeer <Uchunakdamerlan@gmail.com>
2026-04-07 18:59:25 +0700

    Не понятно как это работает надо разабратся

--- app/Model/User.php
+++ app/Model/User.php
@@ -14,7 +14,9 @@ class User extends Model implements IdentityInterface
     protected $fillable = [
         'name',
         'login',
-        'password'
+        'password',
+        'last_name',
+        'surname'
     ];
 
     protected static function booted()
--- views/site/signup.php
+++ views/site/signup.php
@@ -4,5 +4,9 @@
     <label>Имя <input type="text" name="name"></label>
     <label>Логин <input type="text" name="login"></label>
     <label>Пароль <input type="password" name="password"></label>
+    <label>Фамилия <input type="text" name="last_name"></label>
+    <label>Отчество <input type="text" name="surname"></label>
     <button>Зарегистрироваться</button>
+
+
 </form>
