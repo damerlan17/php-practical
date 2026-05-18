4448016d68775a78227121c1692a6cfa868e4145
gr441_uaeer <Uchunakdamerlan@gmail.com>
2026-05-18 10:03:50 +0700

    commit integration php-practical collect

--- app/Middlewares/SpecialCharsMiddleware.php
+++ app/Middlewares/SpecialCharsMiddleware.php
@@ -2,17 +2,17 @@
 namespace Middlewares;
 
 use Src\Request;
+use function Collect\collection;
 
 class SpecialCharsMiddleware
 {
     public function handle(Request $request): Request
     {
-        foreach ($request->all() as $key => $value) {
-            if ($key === 'password') continue;
-            if (is_string($value)) {
-                $request->set($key, htmlspecialchars($value, ENT_QUOTES, 'UTF-8'));
-            }
-        }
-        return $request;   // ВАЖНО!
+        collection($request->all())
+            ->each(function ($value, $key, $request) {
+                $request->set($key, is_string($value) ? htmlspecialchars($value) : $value);
+            }, $request);
+
+        return $request;
     }
 }
